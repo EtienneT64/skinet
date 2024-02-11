3f486c8fb6d716850b393708f79d9793627744b8
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2024-02-11 12:37:57 +0200

    updated

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ToastrModule } from 'ngx-toastr';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
@@ -23,6 +25,6 @@ import { ToastrModule } from 'ngx-toastr';
       preventDuplicates: true,
     }),
   ],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, SectionHeaderComponent],
 })
 export class CoreModule {}
